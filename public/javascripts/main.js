127c90ff2e68bc504421f4084aee6093ceafec42
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2018-01-09 13:35:05 +0200

    Add basic workflow

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,7 @@
 (function($) {
 
   $("#analyze").click(function() {
-    var data = JSON.stringify({ text: $("#input").val() });
+    var data = {text: JSON.stringify($("#input").val())};
     $.post('/analyze', data, function(result, status) {
       $("#output").text(JSON.stringify(result));
     });
--- routes/analyze.js
+++ routes/analyze.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+
+var config = require('../config.json');
+
+var analyzer = new ToneAnalyzerV3({
+  username: config.toneanalyzer.username,
+  password: config.toneanalyzer.password,
+  version_date: config.toneanalyzer.version_date
+});
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  res.json({
-    text: "Hello"
+
+  var text = req.body.text;
+  var params = {
+    'tone_input': { text: text },
+    'content_type': 'application/json'
+  };
+
+  analyzer.tone(params, function(error, response) {
+    if (error) {
+      console.log('Error from Watson: ', error);
+    } else {
+      res.json(response);
+    }
   });
 });
 
