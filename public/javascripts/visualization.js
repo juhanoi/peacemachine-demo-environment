4a7c7743429ec6b6d56957251ba0f225cfe2bff8
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2018-01-10 10:51:48 +0200

    Basic animation for words

--- public/javascripts/visualization.js
+++ public/javascripts/visualization.js
@@ -65,39 +65,55 @@
 
   (function repeatSines() {
     sineBlue
-      .transition().duration(1000)
+      .transition().duration(1200)
       .attr("d", "M -15 0 C -5 -10 5 10 15 0")
-      .transition().duration(1000)
+      .transition().duration(800)
       .attr("d", "M -15 0 C -5 10 5 -10 15 0");
 
     sineRed
-      .transition().duration(1000)
+      .transition().duration(800)
       .attr("d", "M -15 0 C -5 10 5 -10 15 0")
-      .transition().duration(1000)
+      .transition().duration(1200)
       .attr("d", "M -15 0 C -5 -10 5 10 15 0")
       .on("end", repeatSines);
   })();
 
   // Text
-  var words = [ "amet",  "consectetur",  "adipiscing",  "elit",  "In",  "pellentesque",  "enim",  "at",  "enim",  "auctor",  "mollis",  "Vestibulum",  "ante",  "ipsum",  "primis",  "in",  "faucibus",  "orci",  "luctus",  "ultrices",  "posuere",  "cubilia",  "Curae",  "Morbi",  "ultricies",  "orci",  "ipsum",  "maximus",  "accumsan",  "Lorem",  "ipsum",  "dolor",  "sit",  "amet",  "consectetur",  "adipiscing",  "elit",  "In",  "pellentesque",  "enim",  "at",  "enim",  "auctor",  "mollis",  "Vestibulum",  "ante",  "ipsum",  "primis",  "in",  "faucibus",  "orci",  "luctus",  "ultrices",  "posuere",  "cubilia",  "Curae",  "Morbi",  "ultricies",  "orci",  "ipsum",  "maximus",  "accumsan"];
+  var words = [{ text:"amet",color:"#000"}, { text:"consectetur",color:"#000"}, { text:"adipiscing",color:"#00A"}, { text:"elit",color:"#000"}, { text:"In",color:"#00A"}, { text:"pellentesque",color:"#000"}, { text:"enim",color:"#000"}, { text:"at",color:"#A00"}, { text:"enim",color:"#000"}, { text:"auctor",color:"#000"}, { text:"mollis",color:"#A00"}, { text:"Vestibulum",color:"#000"}, { text:"ante",color:"#000"}, { text:"ipsum",color:"#000"}, { text:"primis",color:"#000"}, { text:"in",color:"#00A"}, { text:"faucibus",color:"#000"}, { text:"orci",color:"#000"}, { text:"luctus",color:"#000"}, { text:"ultrices",color:"#A00"}, { text:"posuere",color:"#A00"}, { text:"cubilia",color:"#A00"}, { text:"Curae",color:"#00A"}, { text:"Morbi",color:"#000"}, { text:"ultricies",color:"#000"}, { text:"orci",color:"#000"}, { text:"ipsum",color:"#A00"}, { text:"maximus",color:"#000"}, { text:"accumsan",color:"#000"}, { text:"Lorem",color:"#000"}, { text:"ipsum",color:"#000"}, { text:"dolor",color:"#000"}, { text:"sit",color:"#00A"}, { text:"amet",color:"#000"}, { text:"consectetur",color:"#000"}, { text:"adipiscing",color:"#000"}, { text:"elit",color:"#000"}, { text:"In",color:"#00A"}, { text:"pellentesque",color:"#000"}, { text:"enim",color:"#000"}, { text:"at",color:"#000"}, { text:"enim",color:"#000"}, { text:"auctor",color:"#000"}, { text:"mollis",color:"#000"}, { text:"Vestibulum",color:"#000"}, { text:"ante",color:"#000"}, { text:"ipsum",color:"#000"}, { text:"primis",color:"#A00"}, { text:"in",color:"#000"}, { text:"faucibus",color:"#00A"}, { text:"orci",color:"#00A"}, { text:"luctus",color:"#00A"}, { text:"ultrices",color:"#000"}, { text:"posuere",color:"#000"}, { text:"cubilia",color:"#A00"}, { text:"Curae",color:"#A00"}, { text:"Morbi",color:"#000"}, { text:"ultricies",color:"#000"}, { text:"orci",color:"#000"}, { text:"ipsum",color:"#000"}, { text:"maximus",color:"#00A"}, { text:"accumsan",color:"#000"}];
 
   function wordPosition(i) {
-    var alpha = 360 - 5.8 * i;  // deg
+    var alpha = 5.8 * i;  // deg
     var beta = Math.PI / 180 * alpha; // rad
     var x = 16 * Math.cos(beta);
     var y = 16 * Math.sin(beta);
     return { alpha: alpha, beta: beta, x: x, y: y };
   }
 
-  svg.selectAll("text")
-      .data(words).enter().append("text")
-          .style("font-size", "1px")
-          .style("fill", "#000")
-          .attr("text-anchor", "start")
-          .attr("transform", function (d,i) {
-            var p = wordPosition(i);
-            return "translate(" + [ p.x, p.y ] + ")rotate(" + p.alpha + ")";
-          })
-          .text(function (d) { return d; });
+  (function repeatWords() {
+    svg.selectAll("text")
+        .data(words).enter()
+        .append("text")
+            .style("font-size", "1px")
+            .style("font-weight", function (d,i) { return (d.color === "#000") ? "normal" : "bold"; })
+            .style("fill", function (d,i) { return d.color; })
+            .attr("fill-opacity", "0")
+            .attr("text-anchor", "start")
+            .attr("transform", function (d,i) {
+              var p = wordPosition(i);
+              return "translate(" + [ p.x, p.y ] + ")rotate(" + p.alpha + ")";
+            })
+            .text(function (d) { return d.text; })
+            .transition()
+                .delay(function(d,i) { return 48*i; })
+                .duration(152)
+                .attr("fill-opacity", 1)
+            .transition()
+                .delay(200)
+                .duration(350)
+                .attr("fill-opacity", 0)
+            .remove();
+
+    setTimeout(repeatWords, 61*48+152+550);
+  })();
 
 })();
