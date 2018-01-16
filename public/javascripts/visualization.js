b897f61ce861267ac57dc55fcf25b9db47d2bcdb
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2018-01-16 10:43:38 +0200

    Change data handling

--- public/javascripts/visualization.js
+++ public/javascripts/visualization.js
@@ -29,18 +29,11 @@
       return gradient;
   }
 
-
-  var gradientRed = initializeGradient("Red", "#54ff9f");
-  var gradientBlue = initializeGradient("Blue", "#54ff9f");
-  var gradientGreen = initializeGradient("Green", "#54ff9f");
-  var gradientPurple = initializeGradient("Purple", "#54ff9f");
-
-  /*
   var gradientRed = initializeGradient("Red", "#A00");
   var gradientBlue = initializeGradient("Blue", "#00A");
   var gradientGreen = initializeGradient("Green", "#0A0");
   var gradientPurple = initializeGradient("Purple", "#70A");
-  */
+
 
   // Create the center circle
   var circle = svg.append("circle")
@@ -164,140 +157,15 @@
 
 
   // Text
-  var sentences = [
-    {text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Donec ullamcorper lacus quis auctor sodales. ",anger:0,fear:0.6,joy:0,sadness:0},
-    {text:"In id metus in urna tristique sagittis quis et lacus. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Ut quis nisi non orci pretium bibendum. ",anger:0,fear:0,joy:0,sadness:0.75},
-    {text:"Suspendisse pharetra lorem eu augue dapibus molestie. ",anger:0.2,fear:0.4,joy:0,sadness:0},
-    {text:"Etiam at leo suscipit, semper arcu eget, tempor diam. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Sed non augue eu nisi sodales ornare. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Curabitur non orci ut sem facilisis semper. ",anger:0,fear:0,joy:0.1,sadness:0},
-    {text:"Donec ultrices lectus in turpis sollicitudin sollicitudin. ",anger:0.75,fear:0,joy:0,sadness:0.3},
-    {text:"Cras non justo id enim pulvinar egestas vel sit amet lectus. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Sed vulputate sem eget justo efficitur placerat. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Curabitur a mi iaculis, consectetur elit id, dignissim turpis. ",anger:0,fear:0,joy:0,sadness:0.3},
-    {text:"Integer ut sapien ut leo scelerisque blandit. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Phasellus quis diam tristique lectus consequat dignissim. ",anger:0.2,fear:0,joy:0,sadness:0},
-    {text:"Ut non dolor imperdiet eros dictum iaculis id sed diam. ",anger:0,fear:0,joy:0.9,sadness:0},
-    {text:"Sed vel orci sed mauris bibendum feugiat. ",anger:0,fear:0,joy:0.2,sadness:0},
-    {text:"Donec semper turpis eget lacus hendrerit, aliquet ullamcorper nisi ultricies. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Etiam et diam ac neque fringilla euismod. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Sed eleifend ligula vel ante tempus lobortis. ",anger:0.25,fear:0,joy:0.5,sadness:0},
-    {text:"Fusce sed magna fermentum, blandit augue at, pretium massa. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",anger:0,fear:0.3,joy:0,sadness:0},
-    {text:"Etiam et lectus ac tortor venenatis molestie sit amet nec leo. ",anger:0,fear:0,joy:0.5,sadness:0},
-    {text:"Mauris rhoncus tellus at enim ultrices commodo. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Proin pellentesque orci cursus, aliquam nibh eget, commodo ex. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Aenean eleifend odio suscipit enim faucibus, vel feugiat dolor posuere. ",anger:0,fear:0,joy:0,sadness:0.45},
-    {text:"Aliquam dignissim tellus a lectus fringilla tristique. ",anger:0,fear:0.12,joy:0,sadness:0},
-    {text:"Ut sed velit non erat posuere laoreet ut in arcu. ",anger:0,fear:0,joy:0.67,sadness:0},
-    {text:"Duis et purus vehicula quam fermentum commodo. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Sed sit amet purus ac nunc condimentum eleifend ut vitae est. ",anger:0,fear:0.9,joy:0,sadness:0.2},
-    {text:"Nunc egestas quam sollicitudin turpis varius iaculis. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Nullam tempus augue quis feugiat tempor. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Morbi molestie leo vitae eleifend malesuada. ",anger:0,fear:0.1,joy:0,sadness:0},
-    {text:"Cras imperdiet augue at dignissim maximus. ",anger:0.1,fear:0,joy:0,sadness:0},
-    {text:"Proin tincidunt ante vel posuere tincidunt. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Nunc hendrerit ligula sed eros lobortis varius. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"In tempus mauris id mauris interdum dictum. ",anger:0,fear:0,joy:0.24,sadness:0.56},
-    {text:"Fusce sollicitudin nisi a lobortis gravida. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Quisque eu nisi quis est tincidunt iaculis in eu tortor. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",anger:0,fear:0,joy:0.21,sadness:0},
-    {text:"Vestibulum id sem hendrerit, posuere sapien quis, dapibus nulla. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Integer ut nulla in metus vulputate cursus. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Vivamus vel erat nec urna faucibus tincidunt. ",anger:0,fear:0,joy:0.12,sadness:0},
-    {text:"Proin vulputate risus sed ultricies accumsan. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Praesent eget nibh quis erat hendrerit condimentum eu vestibulum ex. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Aliquam laoreet turpis in diam congue tincidunt. ",anger:0,fear:0,joy:0.12,sadness:0},
-    {text:"Aenean egestas dolor at felis euismod, placerat suscipit ipsum lacinia. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Etiam sit amet elit hendrerit metus finibus egestas. ",anger:0,fear:0,joy:0.2,sadness:0},
-    {text:"Vestibulum a odio congue, placerat tellus quis, ultrices enim. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Curabitur vulputate enim a lorem viverra ullamcorper. ",anger:0.23,fear:0,joy:0,sadness:0.532},
-    {text:"Nullam tempus dui vitae vestibulum aliquam. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Duis faucibus diam sed nunc tincidunt, non dignissim felis tempor. ",anger:0,fear:0.234,joy:0,sadness:0},
-    {text:"Vivamus ornare libero venenatis bibendum malesuada. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Nulla at diam eu mauris posuere dapibus. ",anger:0,fear:0,joy:0,sadness:0.234},
-    {text:"Aliquam varius nisl at cursus commodo. ",anger:0,fear:0.856,joy:0,sadness:0.54},
-    {text:"Nunc sollicitudin purus consectetur ante sodales, sed mollis nisl egestas. ",anger:0,fear:0.345,joy:0,sadness:0},
-    {text:"Phasellus rhoncus turpis ac ante feugiat interdum. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Proin in lectus eu diam eleifend varius. ",anger:0,fear:0.7,joy:0,sadness:0.34},
-    {text:"Phasellus laoreet nulla eu nulla vehicula, vel aliquet arcu fringilla. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"In vel lectus sit amet metus pretium lobortis et nec ligula. ",anger:0,fear:0,joy:0.34,sadness:0.67},
-    {text:"Quisque consectetur lacus rutrum, sagittis urna sit amet, pretium nibh. ",anger:0,fear:0.23,joy:0,sadness:0},
-    {text:"Etiam vestibulum dolor at nulla ullamcorper ornare quis ut enim.",anger:0,fear:0,joy:0,sadness:0.56},
-    {text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Donec ullamcorper lacus quis auctor sodales. ",anger:0,fear:0.6,joy:0,sadness:0},
-    {text:"In id metus in urna tristique sagittis quis et lacus. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Ut quis nisi non orci pretium bibendum. ",anger:0,fear:0,joy:0,sadness:0.75},
-    {text:"Suspendisse pharetra lorem eu augue dapibus molestie. ",anger:0.2,fear:0.4,joy:0,sadness:0},
-    {text:"Etiam at leo suscipit, semper arcu eget, tempor diam. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Sed non augue eu nisi sodales ornare. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Curabitur non orci ut sem facilisis semper. ",anger:0,fear:0,joy:0.1,sadness:0},
-    {text:"Donec ultrices lectus in turpis sollicitudin sollicitudin. ",anger:0.75,fear:0,joy:0,sadness:0.3},
-    {text:"Cras non justo id enim pulvinar egestas vel sit amet lectus. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Sed vulputate sem eget justo efficitur placerat. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Curabitur a mi iaculis, consectetur elit id, dignissim turpis. ",anger:0,fear:0,joy:0,sadness:0.3},
-    {text:"Integer ut sapien ut leo scelerisque blandit. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Phasellus quis diam tristique lectus consequat dignissim. ",anger:0.2,fear:0,joy:0,sadness:0},
-    {text:"Ut non dolor imperdiet eros dictum iaculis id sed diam. ",anger:0,fear:0,joy:0.9,sadness:0},
-    {text:"Sed vel orci sed mauris bibendum feugiat. ",anger:0,fear:0,joy:0.2,sadness:0},
-    {text:"Donec semper turpis eget lacus hendrerit, aliquet ullamcorper nisi ultricies. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Etiam et diam ac neque fringilla euismod. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Sed eleifend ligula vel ante tempus lobortis. ",anger:0.25,fear:0,joy:0.5,sadness:0},
-    {text:"Fusce sed magna fermentum, blandit augue at, pretium massa. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",anger:0,fear:0.3,joy:0,sadness:0},
-    {text:"Etiam et lectus ac tortor venenatis molestie sit amet nec leo. ",anger:0,fear:0,joy:0.5,sadness:0},
-    {text:"Mauris rhoncus tellus at enim ultrices commodo. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Proin pellentesque orci cursus, aliquam nibh eget, commodo ex. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Aenean eleifend odio suscipit enim faucibus, vel feugiat dolor posuere. ",anger:0,fear:0,joy:0,sadness:0.45},
-    {text:"Aliquam dignissim tellus a lectus fringilla tristique. ",anger:0,fear:0.12,joy:0,sadness:0},
-    {text:"Ut sed velit non erat posuere laoreet ut in arcu. ",anger:0,fear:0,joy:0.67,sadness:0},
-    {text:"Duis et purus vehicula quam fermentum commodo. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Sed sit amet purus ac nunc condimentum eleifend ut vitae est. ",anger:0,fear:0.9,joy:0,sadness:0.2},
-    {text:"Nunc egestas quam sollicitudin turpis varius iaculis. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Nullam tempus augue quis feugiat tempor. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Morbi molestie leo vitae eleifend malesuada. ",anger:0,fear:0.1,joy:0,sadness:0},
-    {text:"Cras imperdiet augue at dignissim maximus. ",anger:0.1,fear:0,joy:0,sadness:0},
-    {text:"Proin tincidunt ante vel posuere tincidunt. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Nunc hendrerit ligula sed eros lobortis varius. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"In tempus mauris id mauris interdum dictum. ",anger:0,fear:0,joy:0.24,sadness:0.56},
-    {text:"Fusce sollicitudin nisi a lobortis gravida. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Quisque eu nisi quis est tincidunt iaculis in eu tortor. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",anger:0,fear:0,joy:0.21,sadness:0},
-    {text:"Vestibulum id sem hendrerit, posuere sapien quis, dapibus nulla. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Integer ut nulla in metus vulputate cursus. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Vivamus vel erat nec urna faucibus tincidunt. ",anger:0,fear:0,joy:0.12,sadness:0},
-    {text:"Proin vulputate risus sed ultricies accumsan. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Praesent eget nibh quis erat hendrerit condimentum eu vestibulum ex. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Aliquam laoreet turpis in diam congue tincidunt. ",anger:0,fear:0,joy:0.12,sadness:0},
-    {text:"Aenean egestas dolor at felis euismod, placerat suscipit ipsum lacinia. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Etiam sit amet elit hendrerit metus finibus egestas. ",anger:0,fear:0,joy:0.2,sadness:0},
-    {text:"Vestibulum a odio congue, placerat tellus quis, ultrices enim. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Curabitur vulputate enim a lorem viverra ullamcorper. ",anger:0.23,fear:0,joy:0,sadness:0.532},
-    {text:"Nullam tempus dui vitae vestibulum aliquam. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Duis faucibus diam sed nunc tincidunt, non dignissim felis tempor. ",anger:0,fear:0.234,joy:0,sadness:0},
-    {text:"Vivamus ornare libero venenatis bibendum malesuada. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Nulla at diam eu mauris posuere dapibus. ",anger:0,fear:0,joy:0,sadness:0.234},
-    {text:"Aliquam varius nisl at cursus commodo. ",anger:0,fear:0.856,joy:0,sadness:0.54},
-    {text:"Nunc sollicitudin purus consectetur ante sodales, sed mollis nisl egestas. ",anger:0,fear:0.345,joy:0,sadness:0},
-    {text:"Phasellus rhoncus turpis ac ante feugiat interdum. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"Proin in lectus eu diam eleifend varius. ",anger:0,fear:0.7,joy:0,sadness:0.34},
-    {text:"Phasellus laoreet nulla eu nulla vehicula, vel aliquet arcu fringilla. ",anger:0,fear:0,joy:0,sadness:0},
-    {text:"In vel lectus sit amet metus pretium lobortis et nec ligula. ",anger:0,fear:0,joy:0.34,sadness:0.67},
-    {text:"Quisque consectetur lacus rutrum, sagittis urna sit amet, pretium nibh. ",anger:0,fear:0.23,joy:0,sadness:0},
-    {text:"Etiam vestibulum dolor at nulla ullamcorper ornare quis ut enim.",anger:0,fear:0,joy:0,sadness:0.56}
-  ];
-
   function wordPosition(i) {
-    var alpha = 5.8 * i;  // deg
+    var alpha = 5.8 * i;  // deg // 62 lines can fit one circle
     var beta = Math.PI / 180 * alpha; // rad
     var x = 16 * Math.cos(beta);
     var y = 16 * Math.sin(beta);
     return { alpha: alpha, beta: beta, x: x, y: y };
   }
 
-  (function repeatWords() {
+  function showSentences(sentences, callback) {
     svg.selectAll("text")
         .data(sentences).enter()
         .append("text")
@@ -318,7 +186,7 @@
             })
             .text(function (d) { return d.text; })
             .transition()
-                .delay(function(d,i) { return 48*i; })
+                .delay(function(d,i) { return 200*i; })
                 .duration(152)
                 .attr("fill-opacity", 1)
                 .on("end", function(d,i) {
@@ -328,12 +196,33 @@
                   if (d.sadness > 0) waves[3] = applyForce(waves[3], d.sadness*10);
                 })
             .transition()
-                .delay(200)
-                .duration(350)
+                .delay(1000)
+                .duration(1000)
                 .attr("fill-opacity", 0)
+                .on("end", function(d,i) {
+                  if (svg.selectAll("text").size() === 1) {
+                    callback.call();
+                  }
+                })
             .remove();
+  };
 
-    setTimeout(repeatWords, 6000+120*48+152+550);
-  })();
+  (function readFiles(index) {
+    var files = [
+      "/data/lorem.json",
+      "/data/lorem2.json"
+    ];
+
+    d3.json(files[index], function (data) {
+      showSentences(data, function() {
+        if (index === files.length-1) {
+          setTimeout(() => readFiles(0), 1000);
+        } else {
+          setTimeout(() => readFiles(index+1), 1000);
+        }
+      });
+    });
+  })(0);
+
 
 })();
