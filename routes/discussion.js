3a9417d61e5826dd2357fdd69eea787494a1ef4e
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2018-02-06 09:29:55 +0200

    Add basic visualizations for discussiosn

--- routes/discussion.js
+++ routes/discussion.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var discussions = {
+  raw: require('../public/data/teemus-discussion.json'),
+  watson: require('../public/data/teemus-discussion-watson.json'),
+  human: require('../public/data/teemus-discussion-human.json')
+};
+console.log(discussions.watson)
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('discussion', { title: 'Peace Machine - Discussion' });
+  res.render('discussion', { title: 'Peace Machine - Discussion', discussions: discussions });
 });
 
 module.exports = router;
