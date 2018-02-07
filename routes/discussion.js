ca759bb3916ea91bc877c78e534a33606096c7ce
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2018-02-07 13:03:39 +0200

    Add improved analysis

--- routes/discussion.js
+++ routes/discussion.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 var discussions = {
   raw: require('../public/data/teemus-discussion.json'),
-  watson: require('../public/data/teemus-discussion-watson.json'),
+  watson: require('../public/data/teemus-discussion-watson-line-by-line.json'),
   human: require('../public/data/teemus-discussion-human.json')
 };
-console.log(discussions.watson)
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('discussion', { title: 'Peace Machine - Discussion', discussions: discussions });
